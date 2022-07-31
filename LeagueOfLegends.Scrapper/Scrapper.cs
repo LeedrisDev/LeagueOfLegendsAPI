eb2b3eb64b7b0d86b7c97d39898d93596799fe4d
Thomas Blondel <tfoenix@gmail.com>
2022-07-31 18:57:10 +0200

    [Start] Added Scrapper, base API and docker support

---
+++ LeagueOfLegends.Scrapper/Scrapper.cs
@@ -0,0 +1,69 @@
+﻿namespace LeagueOfLegends.Scrapper;
+
+using HtmlAgilityPack;
+using System.Net.Http;
+using Model;
+
+public class Scrapper
+{
+    private static async Task<string> GetUrl(string fullUrl)
+    {
+        var client = new HttpClient();
+        var response = await client.GetStringAsync(fullUrl);
+        return response;
+    }
+
+    private static IEnumerable<PatchNoteModel> ParseHtml(string html)
+    {
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(html);
+
+        var patchNoteNames = htmlDoc.DocumentNode.Descendants("h2")
+            .Where(node => node.GetAttributeValue("class", "").Equals("style__Title-sc-1h41bzo-8 fEywOQ"))
+            .Select(node => node.InnerText)
+            .ToList();
+
+        var imageUrls = htmlDoc.DocumentNode.Descendants("img")
+            .Where(node =>
+                node.GetAttributeValue("class", "")
+                    .Equals("style__NoScriptImg-g183su-0 style__Img-g183su-1 cipsic dBitJH"))
+            .Select(node => node.GetAttributeValue("src", ""))
+            .ToList();
+
+        var patchNotesUrl = htmlDoc.DocumentNode.Descendants("a")
+            .Where(node =>
+                node.GetAttributeValue("class", "")
+                    .Equals(
+                        "style__Wrapper-sc-1h41bzo-0 style__ResponsiveWrapper-sc-1h41bzo-13 jyxTUP cayvOp isVisible"))
+            .Select(node => node.GetAttributeValue("href", ""))
+            .ToList();
+
+        if (patchNoteNames.Count() != imageUrls.Count() || patchNoteNames.Count() != patchNotesUrl.Count())
+        {
+            throw new ApplicationException(
+                "PatchNotesNames, imageUrls and / or patchNotesUrl doesn't have the same length");
+        }
+
+        var patchNotes = new List<PatchNoteModel>();
+        for (var i = 0; i < patchNoteNames.Count; i++)
+        {
+            var patchNote = new PatchNoteModel(patchNoteNames[i], imageUrls[i], patchNotesUrl[i]);
+            patchNotes.Add(patchNote);
+        }
+
+        return patchNotes;
+    }
+
+    public static async Task<string> ParseRiotPageNotesPage()
+    { 
+        const string url = "https://www.leagueoflegends.com/fr-fr/news/tags/patch-notes/";
+        var response = await GetUrl(url);
+        var patchNotes = ParseHtml(response);
+
+        foreach (var patchNote in patchNotes)
+        {
+            Console.WriteLine("Name: {0} \t imageUrl: {1} \t patchNoteUrl: {2}", patchNote.Version, patchNote.ImageUrl, patchNote.PatchNoteUrl );
+        }
+        return response;
+    }
+}
