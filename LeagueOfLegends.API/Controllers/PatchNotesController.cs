dcf9896cddaeea0a9246e8c89091df6275c2a899
Thomas Blondel <tfoenix@gmail.com>
2022-08-04 19:49:04 +0200

    [Endpoint] Added new route to get Teamfight Tactics patchs

--- LeagueOfLegends.API/Controllers/PatchNotesController.cs
+++ LeagueOfLegends.API/Controllers/PatchNotesController.cs
@@ -21,18 +21,27 @@ public class PatchNotesController: ControllerBase
         _ctx = ctx;
     }
 
-    /// <summary>Get the last 6 patch notes.</summary>
-    /// <returns>Last 6 patch notes</returns>
-    /// <response code="200">Returns the last 6 patch notes</response>
+    /// <summary>Get the last 6 League Of Legends patch notes.</summary>
+    /// <returns>Last 6 League Of Legends patch notes</returns>
+    /// <response code="200">Returns the last 6 League Of Legends patch notes</response>
     [HttpGet("LeagueOfLegends")]
-    public async Task<IActionResult> Get()
+    public async Task<IActionResult> GetLeagueOfLegendsPatchNotes()
     {
         var patchNotes = await _patchNotesScrapper.ParseRiotPageNotesPage();
         await _ctx.PatchNoteModels.AddRangeAsync(patchNotes);
         await _ctx.SaveChangesAsync();
         return Ok(patchNotes);
     }
-    
+
+    /// <summary>Get the last 6 Teamfight Tactics patch notes.</summary>
+    /// <returns>Last 6 Teamfight Tactics patch notes</returns>
+    /// <response code="200">Returns the last 6 Teamfight Tactics patch notes</response>
+    [HttpGet("TeamfightTactics")]
+    public async Task<IActionResult> GetTeamfightTacticsPatchNotes()
+    {
+        return Ok("Hello world");
+    }
+
     [HttpGet("GetAllPatchnotesFromDatabase")]
     public async Task<IActionResult> GetAll()
     {
